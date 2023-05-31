ccca069d8c5bc54cc9e2d61acf47716a4e21a4a4
Jared Stratton <Stratton_10@hotmail.com>
2023-05-30 21:30:37 -0300

    Quiz questions and answers bank for quiz

---
+++ assets/js/question-bank.js
@@ -0,0 +1,53 @@
+// Quiz Questions + Answers bank below:
+const qabank = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answer: "<script>",
+        choices: ["<js>", "<scripting>", "<javascript>", "<script>"]
+    },
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+        answer: "Both the <head> section and the <body> section are correct",
+        choices: ["The <head> section", "Both the <head> section and the <body> section are correct", "The <body> section", "Neither"]
+    },
+    {
+        question: "How to write an IF statement in JavaScript?",
+        answer: "if (i == 5)",
+        choices: ["if i = 5", "if (i == 5)", "if i == 5 then", "if i = 5 then"]
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        answer: "function myFunction()",
+        choices: ["function:myFunction()", "function myFunction()", "function = myFunction()", "None of the options"]
+    },
+    {
+        question: "How does a FOR loop start?",
+        answer: "for (i = 0; i <= 5; i++)",
+        choices: ["for (i = 0; i <= 5; i++)", "for i = 1 to 5", "for (i <= 5; i++)", "for (i = 0; i <= 5)"]
+    },
+    {
+        question: "How can you add a comment in a JavaScript?",
+        answer: "//This is a comment",
+        choices: ["//This is a comment", "'This is a comment", "<!--This is a comment-->", "*This is a comment"]
+    },
+    {
+        question: "How does a WHILE loop start?",
+        answer: "while (i <= 10)",
+        choices: ["while (i <= 10; i++)", "while (i <= 10)", "while i = 1 to 10", "while (i == 10)"]
+    },
+    {
+        question: "What will the following code return: Boolean(10 > 9)?",
+        answer: "true",
+        choices: ["true", "NaN", "false", "NA"]
+    },
+    {
+        question: "Which operator is used to assign a value to a variable?",
+        answer: "=",
+        choices: ["*", "-", "x", "="]
+    },
+    {
+        question: "How do you round the number 7.25, to the nearest integer?",
+        answer: "Math.round(7.25)",
+        choices: ["Math.rnd(7.25)", "round(7.25)", "Math.round(7.25)", "rnd(7.25)"]
+    }
+];
